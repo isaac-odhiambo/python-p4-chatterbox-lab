d6538f47f30262879862f6d8f007165bdc8dfffa
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-10-12 00:36:55 +0300

    done

--- server/models.py
+++ server/models.py
@@ -8,7 +8,48 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+class Bakery(db.Model, SerializerMixin):
+    __tablename__ = 'bakeries'
+
+    serialize_rules = ('-baked_goods.bakery',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    baked_goods = db.relationship('BakedGood', backref='bakery')
+
+    def __repr__(self):
+        return f'<Bakery {self.name}>'
+
+class BakedGood(db.Model, SerializerMixin):
+    __tablename__ = 'baked_goods'
+
+    serialize_rules = ('-bakery.baked_goods',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+
+    def __repr__(self):
+        return f'<Baked Good {self.name}, ${self.price}>'
+
+# New Message model
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
+    serialize_rules = ('-created_at', '-updated_at')
+
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<Message from {self.username}: {self.body}>'
